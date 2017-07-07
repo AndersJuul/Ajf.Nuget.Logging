c0df04f83ed8b00e5474d8fc6e60287d3013f31d
AndersJuul <andersjuulsfirma@gmail.com>
2017-07-07 12:24:53 +0200

    a

--- Src/Ajf.Nuget.Logging/StandardLoggerProvider.cs
+++ Src/Ajf.Nuget.Logging/StandardLoggerProvider.cs
@@ -1,12 +1,22 @@
-﻿using Serilog;
+﻿using System;
+using System.Configuration;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace Ajf.Nuget.Logging
 {
-    public class StandardLoggerProvider 
+    public class StandardLoggerProvider
     {
+        private static ILogger _configuredLogger;
         public static ILogger GetLogger()
         {
-            return Log.Logger;
+            if(_configuredLogger==null)
+                _configuredLogger = new LoggerConfiguration()
+                .WriteTo.RollingFile(ConfigurationManager.AppSettings["RollingFile"])
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(ConfigurationManager.AppSettings["EsLoggingUrl"])))
+                .CreateLogger();
+
+            return _configuredLogger;
         }
     }
 }
