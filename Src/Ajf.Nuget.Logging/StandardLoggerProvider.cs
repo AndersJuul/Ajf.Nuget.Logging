52a705d43227830fa3f3c1008dc81bf17dd399f0
AndersJuul <andersjuulsfirma@gmail.com>
2017-07-07 12:32:41 +0200

    a

--- Src/Ajf.Nuget.Logging/StandardLoggerProvider.cs
+++ Src/Ajf.Nuget.Logging/StandardLoggerProvider.cs
@@ -8,13 +8,24 @@ namespace Ajf.Nuget.Logging
     public class StandardLoggerProvider
     {
         private static ILogger _configuredLogger;
+
         public static ILogger GetLogger()
         {
-            if(_configuredLogger==null)
+            if (_configuredLogger == null)
+            {
+                var fileName = ConfigurationManager.AppSettings["LogFileDirectory"] +
+                                 ConfigurationManager.AppSettings["SuiteName"] + "." +
+                                 ConfigurationManager.AppSettings["ComponentName"] + ".log";
+
+                var esLoggingUrl = ConfigurationManager.AppSettings["EsLoggingUrl"];
+
+                var esLoggingUri = new Uri(esLoggingUrl);
+
                 _configuredLogger = new LoggerConfiguration()
-                .WriteTo.RollingFile(ConfigurationManager.AppSettings["RollingFile"])
-                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(ConfigurationManager.AppSettings["EsLoggingUrl"])))
-                .CreateLogger();
+                    .WriteTo.RollingFile(fileName)
+                    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(esLoggingUri))
+                    .CreateLogger();
+            }
 
             return _configuredLogger;
         }
