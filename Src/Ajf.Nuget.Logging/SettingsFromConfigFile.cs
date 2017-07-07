bff06c57029f327f643467ee402d4c6a7fcc0394
AndersJuul <andersjuulsfirma@gmail.com>
2017-07-07 13:14:04 +0200

    a

---
+++ Src/Ajf.Nuget.Logging/SettingsFromConfigFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using Serilog.Sinks.Elasticsearch;
+
+namespace Ajf.Nuget.Logging
+{
+    public class SettingsFromConfigFile
+    {
+        public SettingsFromConfigFile()
+        {
+            Environment = ConfigurationManager.AppSettings["Environment"];
+            SuiteName = ConfigurationManager.AppSettings["SuiteName"];
+            ComponentName = ConfigurationManager.AppSettings["ComponentName"];
+            LogFileDirectory = ConfigurationManager.AppSettings["LogFileDirectory"];
+            EsLoggingUrl = ConfigurationManager.AppSettings["EsLoggingUrl"];
+            ReleaseNumber = ConfigurationManager.AppSettings["ReleaseNumber"];
+
+            FileName = $"{LogFileDirectory}{SuiteName}.{ComponentName}.log";
+
+            EsLoggingUri = new Uri(EsLoggingUrl);
+            ElasticsearchSinkOptions = new ElasticsearchSinkOptions(EsLoggingUri);
+        }
+
+        public string LogFileDirectory { get; set; }
+
+        public string ReleaseNumber { get; set; }
+        public string ComponentName { get; set; }
+        public string SuiteName { get; set; }
+        public string FileName { get; set; }
+        public string EsLoggingUrl { get; set; }
+        public Uri EsLoggingUri { get; set; }
+        public ElasticsearchSinkOptions ElasticsearchSinkOptions { get; set; }
+        public string Environment { get; set; }
+    }
+}
---
+++ Src/Ajf.Nuget.Logging/StandardLoggerConfigurator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Serilog;
+using Serilog.Core.Enrichers;
+
+namespace Ajf.Nuget.Logging
+{
+    public class StandardLoggerConfigurator
+    {
+        private static IDisposable LogContext { get; set; }
+
+        public static void SetLogger()
+        {
+            var settings = new SettingsFromConfigFile();
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.RollingFile(settings.FileName)
+                .WriteTo.Elasticsearch(settings.ElasticsearchSinkOptions)
+                .CreateLogger();
+
+            // LogContext is 'remembered' because the properties are applied until this object is recycled.
+            LogContext = Serilog.Context.LogContext.Push(new PropertyEnricher("Environment", settings.Environment),
+                new PropertyEnricher("SuiteName", settings.SuiteName),
+                new PropertyEnricher("ComponentName", settings.ComponentName),
+                new PropertyEnricher("ReleaseNumber", settings.ReleaseNumber));
+        }
+    }
+}
