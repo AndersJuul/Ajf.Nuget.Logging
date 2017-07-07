d8db91563d38cfeb36bdfcb568e3fb9c81703d2f
AndersJuul <andersjuulsfirma@gmail.com>
2017-07-07 14:39:10 +0200

    a

---
+++ Src/Ajf.Nuget.Logging/SettingsEnricher.cs
@@ -0,0 +1,17 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace Ajf.Nuget.Logging
+{
+    public class SettingsEnricher: ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var settings = new SettingsFromConfigFile();
+            propertyFactory.CreateProperty("Environment", settings.Environment);
+            propertyFactory.CreateProperty("SuiteName", settings.SuiteName);
+            propertyFactory.CreateProperty("ComponentName", settings.ComponentName);
+            propertyFactory.CreateProperty("ReleaseNumber", settings.ReleaseNumber);
+        }
+    }
+}
--- Src/Ajf.Nuget.Logging/StandardLoggerConfigurator.cs
+++ Src/Ajf.Nuget.Logging/StandardLoggerConfigurator.cs
@@ -6,28 +6,16 @@ namespace Ajf.Nuget.Logging
 {
     public class StandardLoggerConfigurator
     {
-        public static ILogger GetLogger()
+        public static ILogger GetEnrichedLogger()
         {
             var settings = new SettingsFromConfigFile();
 
             return new LoggerConfiguration()
+                .Enrich.With<SettingsEnricher>()
                 .Enrich.FromLogContext()
                 .WriteTo.RollingFile(settings.FileName)
                 .WriteTo.Elasticsearch(settings.ElasticsearchSinkOptions)
                 .CreateLogger();
-        }
-
-        public static IDisposable GetLogContext()
-        {
-            var settings = new SettingsFromConfigFile();
-            // LogContext is 'remembered' because the properties are applied until this object is recycled.
-            var LogContext = Serilog.Context.LogContext.Push(
-                new PropertyEnricher("Environment", settings.Environment),
-                new PropertyEnricher("SuiteName", settings.SuiteName),
-                new PropertyEnricher("ComponentName", settings.ComponentName),
-                new PropertyEnricher("ReleaseNumber", settings.ReleaseNumber));
-
-            return LogContext;
         }
     }
 }
