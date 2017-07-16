62eb3e76333ea0e4df8fbfec31b7dd9f70f9bea7
Anders Juul <anders_privat@hotmail.com>
2017-07-16 12:03:31 +0200

    a

---
+++ Src/Ajf.Nuget.Logging/ServiceSettingsFromConfigFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+
+namespace Ajf.Nuget.Logging
+{
+    public class ServiceSettingsFromConfigFile: SettingsFromConfigFile
+    {
+        public ServiceSettingsFromConfigFile()
+        {
+            Description = ConfigurationManager.AppSettings["Description"];
+            DisplayName = ConfigurationManager.AppSettings["DisplayName"];
+            ServiceName = ConfigurationManager.AppSettings["ServiceName"];
+
+            if (string.IsNullOrEmpty(Description))
+                throw new ArgumentException("AppSetting can't be null/empty", nameof(Description));
+            if (string.IsNullOrEmpty(DisplayName))
+                throw new ArgumentException("AppSetting can't be null/empty", nameof(DisplayName));
+            if (string.IsNullOrEmpty(ServiceName))
+                throw new ArgumentException("AppSetting can't be null/empty", nameof(ServiceName));
+        }
+
+        public string ServiceName { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Description { get; set; }
+    }
+}
