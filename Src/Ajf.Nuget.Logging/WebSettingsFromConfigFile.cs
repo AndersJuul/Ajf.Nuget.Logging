806a3d8df7222c5b74ed3cd9798aa99516248aef
Anders Juul <anders_privat@hotmail.com>
2017-07-16 20:01:44 +0200

    a

---
+++ Src/Ajf.Nuget.Logging/WebSettingsFromConfigFile.cs
@@ -0,0 +1,25 @@
+﻿namespace Ajf.Nuget.Logging
+{
+    public class WebSettingsFromConfigFile : SettingsFromConfigFile
+    {
+        public WebSettingsFromConfigFile()
+        {
+            //Description = ConfigurationManager.AppSettings["Description"];
+            //DisplayName = ConfigurationManager.AppSettings["DisplayName"];
+            //ServiceName = ConfigurationManager.AppSettings["ServiceName"];
+
+            //if (string.IsNullOrEmpty(Description))
+            //    Description = $"{SuiteName}.{ComponentName}.{ReleaseNumber}";
+            //if (string.IsNullOrEmpty(DisplayName))
+            //    DisplayName = $"{SuiteName}.{ComponentName}";
+            //if (string.IsNullOrEmpty(ServiceName))
+            //    ServiceName = $"{SuiteName}.{ComponentName}";
+        }
+
+        //public string ServiceName { get; set; }
+
+        //public string DisplayName { get; set; }
+
+        //public string Description { get; set; }
+    }
+}
