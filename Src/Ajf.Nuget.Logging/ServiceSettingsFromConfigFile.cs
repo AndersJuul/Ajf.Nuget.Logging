8be87c02092bc543e3590a49e7ccf19227742417
Anders Juul <anders_privat@hotmail.com>
2017-07-22 09:48:46 +0200

    a

--- Src/Ajf.Nuget.Logging/ServiceSettingsFromConfigFile.cs
+++ Src/Ajf.Nuget.Logging/ServiceSettingsFromConfigFile.cs
@@ -12,7 +12,6 @@ namespace Ajf.Nuget.Logging
             ServiceName = ConfigurationManager.AppSettings["ServiceName"];
             RunAsUserName = ConfigurationManager.AppSettings["RunAsUserName"];
             RunAsPassword = ConfigurationManager.AppSettings["RunAsPassword"];
-            EasyNetQConfig = ConfigurationManager.AppSettings["EasyNetQConfig"];
 
             if (string.IsNullOrEmpty(RunAsUserName))
                 throw new ArgumentException("AppSetting can't be null/empty", nameof(RunAsUserName));
@@ -37,7 +36,5 @@ namespace Ajf.Nuget.Logging
         public string DisplayName { get; set; }
 
         public string Description { get; set; }
-
-        public string EasyNetQConfig { get; set; }
     }
 }
