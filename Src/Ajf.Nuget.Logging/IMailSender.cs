655033c866462bdd355c246cedcb955ab5cc5e5f
Anders Juul <anders_privat@hotmail.com>
2017-07-26 21:24:35 +0200

    a

--- Src/Ajf.Nuget.Logging/IMailSender.cs
+++ Src/Ajf.Nuget.Logging/IMailSender.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Ajf.Nuget.Logging
 {
     public interface IMailSender
     {
-        Task SendMailAsync(string toAddress, string ccAddress, string senderAddress, string subject, string bodyAsHtml, IEnumerable<string> attachmentPaths);
+        Task<HttpStatusCode> SendMailAsync(string toAddress, string ccAddress, string senderAddress, string subject, string bodyAsHtml, IEnumerable<string> attachmentPaths);
     }
 }
--- Src/Ajf.Nuget.Logging/MailSender.cs
+++ Src/Ajf.Nuget.Logging/MailSender.cs
@@ -5,42 +5,25 @@ using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using SendGrid;
+using SendGrid.Helpers.Mail;
+using Serilog;
 
 namespace Ajf.Nuget.Logging
 {
     public class MailSender: IMailSender
     {
-        public async Task SendMailAsync(string toAddress, string ccAddress, string senderAddress, string subject, string bodyAsHtml, IEnumerable<string> attachmentPaths)
+        public async Task<HttpStatusCode> SendMailAsync(string toAddress, string ccAddress, string senderAddress, string subject, string bodyAsHtml, IEnumerable<string> attachmentPaths)
         {
-            //var folderContents = GetFolderContents(path, path).ToArray();
-            
-            var fromMailAddress = new MailAddress(senderAddress);
-            var networkCredential = new NetworkCredential("azure_9ce305d9c6ab84b3d5fb7723deca51f5@azure.com",
-                "6oi3vEdJl5GldVc");
-            var transport = new Web(networkCredential);
+            Log.Logger.Information("SendMailAsync-");
 
-            // Create the email object first, then add the properties.
-            var myMessage = new SendGridMessage
-            {
-                From = fromMailAddress
-            };
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(senderAddress, "Sender");
 
-            if(!string.IsNullOrEmpty(toAddress))
-            myMessage.AddTo(toAddress);
-            if (!string.IsNullOrEmpty(ccAddress))
-                myMessage.AddCc(ccAddress);
-
-            var bytes = Encoding.Default.GetBytes(subject);
-            myMessage.Subject = Encoding.UTF8.GetString(bytes);
-            myMessage.Html = bodyAsHtml;
-
-            foreach (var attachmentPath in attachmentPaths)
-            {
-                myMessage.AddAttachment(attachmentPath);
-            }
-
-            // Send the email.
-            await transport.DeliverAsync(myMessage).ConfigureAwait(false);
+            var to = new EmailAddress(toAddress, "Example User");
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "Read as html", bodyAsHtml);
+            var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
+            return response.StatusCode;
         }
     }
 }
