73351a9ade2c23950c5f1cbcac6f32927caf1716
Anders Juul <anders_privat@hotmail.com>
2017-07-27 07:57:03 +0200

    a

--- Src/Ajf.Nuget.Logging/MailSender.cs
+++ Src/Ajf.Nuget.Logging/MailSender.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
-using System.Net.Mail;
-using System.Text;
 using System.Threading.Tasks;
 using SendGrid;
 using SendGrid.Helpers.Mail;
@@ -10,13 +8,14 @@ using Serilog;
 
 namespace Ajf.Nuget.Logging
 {
-    public class MailSender: IMailSender
+    public class MailSender : IMailSender
     {
-        public async Task<HttpStatusCode> SendMailAsync(string toAddress, string ccAddress, string senderAddress, string subject, string bodyAsHtml, IEnumerable<string> attachmentPaths)
+        public async Task<HttpStatusCode> SendMailAsync(string toAddress, string ccAddress, string senderAddress,
+            string subject, string bodyAsHtml, IEnumerable<string> attachmentPaths)
         {
             Log.Logger.Information("SendMailAsync-");
 
-            var apiKey = "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";
+            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_KEY");
             var client = new SendGridClient(apiKey);
             var from = new EmailAddress(senderAddress);
 
