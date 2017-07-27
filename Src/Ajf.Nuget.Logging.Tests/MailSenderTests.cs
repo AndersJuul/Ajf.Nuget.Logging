2808c4c3c659cb588db95b4c7df6528452abd5d7
Anders Juul <anders_privat@hotmail.com>
2017-07-27 09:39:00 +0200

    a

---
+++ Src/Ajf.Nuget.Logging.Tests/MailSenderTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using NUnit.Framework;
+
+namespace Ajf.Nuget.Logging.Tests
+{
+    [TestFixture]
+    public class MailSenderTests
+    {
+        [Test]
+        public void TestThatMailCanBeSend()
+        {
+            var mailSender = new MailSender();
+            var httpStatusCode = mailSender.SendMailAsync("andersjuulsfirma@gmail.com", "andersjuulsfirma@gmail.com",
+                "andersjuulsfirma@gmail.com", "Subject", "<html><html>", new string[] { }).Result;
+
+            Assert.AreEqual(HttpStatusCode.Accepted, httpStatusCode);
+        }
+    }
+}
